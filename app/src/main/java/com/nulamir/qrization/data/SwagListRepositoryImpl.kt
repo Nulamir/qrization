9de31110241024fe0f657cd13a135a69098da8f2
nulamir <nulamir@gmail.com>
2023-10-18 09:32:01 +0300

    11

--- app/src/main/java/com/nulamir/qrization/data/SwagListRepositoryImpl.kt
+++ app/src/main/java/com/nulamir/qrization/data/SwagListRepositoryImpl.kt
@@ -1,10 +1,18 @@
 package com.nulamir.qrization.data
 
+import android.os.Build
+import android.os.Environment
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.nulamir.qrization.Manifest
 import com.nulamir.qrization.domain.SwagItem
 import com.nulamir.qrization.domain.SwagListRepository
-import java.lang.RuntimeException
+import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.File
+import java.io.FileInputStream
+
 
 object SwagListRepositoryImpl : SwagListRepository {
 
@@ -19,6 +27,7 @@ object SwagListRepositoryImpl : SwagListRepository {
             val item = SwagItem("Монитор HP TFT Z23i (23\") LED AH-IPS Monitor (250 cd/m2,1000:1,8 ms,178/178,VGA,DVI-D,DisplayPort,US", "МЦ0018680", "202", "201" , "IT"  )
             addSwagItem(item)
         }
+        readSwagListFromExcel()
     }
 
     override fun addSwagItem(swagItem: SwagItem) {
@@ -58,26 +67,38 @@ object SwagListRepositoryImpl : SwagListRepository {
     }
 
     override fun readSwagListFromExcel() {
-//        fun `when file is created then content is correct`() {
-//            val workbook = XSSFWorkbook()
-//            val workSheet = workbook.createSheet()
-//            val cellStyle = workbook.createCellStyle()
-//            cellStyle.fillForegroundColor = IndexedColors.RED.getIndex()
-//            cellStyle.fillPattern = FillPatternType.SOLID_FOREGROUND
-//            val firstCell = workSheet
-//                .createRow(0)
-//                .createCell(0)
-//            firstCell.setCellValue("SAVED VALUE")
-//            firstCell.cellStyle = cellStyle
-//
-//            val tempFile = createTempFile("test_output_", ".xlsx")
-//            workbook.write(tempFile.outputStream())
-//            workbook.close()
-//
-//            val inputWorkbook = WorkbookFactory.create(tempFile.toFile().inputStream())
-//            val firstSheet = inputWorkbook.getSheetAt(0)
-//            assertThat(firstSheet.getRow(0).getCell(0).stringCellValue).isEqualTo("SAVED VALUE")
-//        }
+
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        val filepath = "/storage/emulated/0/Download/list.xls"
+
+
+        try{
+            val inputStream = FileInputStream(filepath)
+
+            var xlWb = WorkbookFactory.create(inputStream)
+            //Row index specifies the row in the worksheet (starting at 0):
+            val rowNumber = 0
+            //Cell index specifies the column within the chosen row (starting at 0):
+            val columnNumber = 0
+            //Get reference to first sheet:
+            val xlWs = xlWb.getSheetAt(0)
+            val xlRows = xlWs.lastRowNum
+            var i=0;
+            while (i<= xlRows){
+                println(xlWs.getRow(i).getCell(4))
+                i++
+            }
+
+        } catch (exception: Exception){
+            println(exception.message)
+        }
+
+
+        //Instantiate Excel workbook using existing file:
+
+
+
     }
 
 }
